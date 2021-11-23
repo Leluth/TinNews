937e71ce7830d95b29a532ba8df8e2244224d57b
Shaoshuai-Xu <xu.shaosh@northeastern.edu>
2021-11-22 21:03:45 -0800

    [Add] Add bottom navigation bar

--- app/src/main/java/com/laioffer/tinnews/MainActivity.java
+++ app/src/main/java/com/laioffer/tinnews/MainActivity.java
@@ -1,14 +1,34 @@
 package com.laioffer.tinnews;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        assert navHostFragment != null;
+        navController = navHostFragment.getNavController();
+        // bind navigation graph with bottom navigation view
+        NavigationUI.setupWithNavController(navView, navController);
+        // bind navigation graph with top action bar
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return navController.navigateUp();
     }
 }
---
+++ app/src/main/java/com/laioffer/tinnews/model/NewsResponse.java
@@ -0,0 +1,42 @@
+package com.laioffer.tinnews.model;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+import java.util.Objects;
+
+public class NewsResponse {
+    public Integer totalResults;
+    public List<Article> articles;
+    public String code;
+    public String message;
+    public String status;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NewsResponse that = (NewsResponse) o;
+        return Objects.equals(totalResults, that.totalResults)
+                && Objects.equals(articles, that.articles) && Objects.equals(code, that.code)
+                && Objects.equals(message, that.message) && Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(totalResults, articles, code, message, status);
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "NewsResponse{" +
+                "totalResults=" + totalResults +
+                ", articles=" + articles +
+                ", code='" + code + '\'' +
+                ", message='" + message + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
+// 0a8960e73a554712b217e2f2fd026174
